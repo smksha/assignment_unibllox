903fc8c50cad0bc791fa5bb226dbb364887dd207
Sameeksha Gupta <sameekshagupta.11@gmail.com>
2026-01-25 16:02:48 +0530

    chore: stabilize frontend setup with Vite 5, Tailwind v3, Node 18

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-600">
+          Tailwind is working 🚀
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
