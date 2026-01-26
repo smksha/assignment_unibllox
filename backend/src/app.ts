3f20bc13a174916009aae38082020791b2d5a08c
Sameeksha Gupta <sameekshagupta.11@gmail.com>
2026-01-26 11:07:27 +0530

    fix: cors issue

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,12 @@ import express from "express";
 import cartRoutes from "./routes/cart.routes";
 import checkoutRoutes from "./routes/checkout.routes";
 import adminRoutes from "./routes/admin.routes";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.use(express.json());
 
