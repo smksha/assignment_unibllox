123ef559668826032972062aac2a60c9405145d4
Sameeksha Gupta <sameekshagupta.11@gmail.com>
2026-01-28 12:37:21 +0530

    fix: buildissues

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import { CartProvider } from "./context/CartContext.tsx";
+import App from "./App";
+import { CartProvider } from "./context/CartContext";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
